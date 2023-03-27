c0e13072b6fdfb9d33aaafd9c27f1bb28d921ebb
dleeftink <dennisleeftink@gmail.com>
2023-03-28 00:52:18 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+const express = require("express");
+const app = express();
+
+app.all("/", async (req, res) => {
+    
+  const data = await fetch("http://ipwho.is").then((d) => d.json());
+  const headers = {
+    "Access-Control-Allow-Origin": req.headers.origin || "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Allow-Headers": "*",
+    "Content-Type": "text/plain",
+  };
+
+  res.writeHead(200, headers);
+  res.end(JSON.stringify({ ...data }));
+  // res.end(JSON.stringify({ time: Date.now(), context:{answer:true } }));
+  // res.send({...data})
+    
+});
+
 app.listen(process.env.PORT || 3000)
